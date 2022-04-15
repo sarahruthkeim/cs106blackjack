c19df1443c7f41ab654e45cb8199dc8e49f12705
JustinReed <jreed1@haverford.edu>
2022-04-15 10:21:53 -0400

    done with card class;

---
+++ Card.java
@@ -0,0 +1,38 @@
+public class Card {
+    enum Suit {HEARTS, DIAMONDS, CLUBS, SPADES};
+    enum Face {ACE, JACK, QUEEN, KING};
+
+    private Suit suit;
+    private int cardType;
+    private int value;
+
+    public Card (Suit inputSuit, int inputCardType) {
+        this.suit = inputSuit;
+        this.cardType = inputCardType;
+
+        if (cardType == 1) {
+            this.value = inputCardType; // ace can be worth 1 or 11, will need to implement a call to the user to choose
+            // what its worth
+        }  else if (cardType > 10){
+            this.value = 10;
+        }else {
+            this.value = inputCardType;
+        }
+
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+    public int getCardType() {
+        return this.cardType;
+    }
+
+    public void updateAceValue() {
+        if (cardType == 1) {
+            value += 10;
+            value = value % 20;
+        }
+    }
+}
