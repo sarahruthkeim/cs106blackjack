98cc8e6b7f01d0070308154d578ef65d59c5477c
JustinReed <jreed1@haverford.edu>
2022-04-22 11:51:02 -0400

    saving

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main (String[] args) {
 
@@ -6,13 +8,28 @@ public class Main {
         Player player3 = new Player();
         Player player4 = new Player();
 
-        CircularlyLinkedList<Player> playerListIndex = new CircularlyLinkedList<Player>();
-        playerListIndex.addLast(player1);
-        playerListIndex.addLast(player2);
-        playerListIndex.addLast(player3);
-        playerListIndex.addLast(player4);
+        ArrayList<Player> playerListIndex = new ArrayList<Player>();
+        int roundNumber = 0;
 
-        CircularlyLinkedList<Player> gamePlayerList =
+        playerListIndex.add(player1);
+        playerListIndex.add(player2);
+        playerListIndex.add(player3);
+        playerListIndex.add(player4);
+
+        CircularlyLinkedList<Player> gamePlayerList = new CircularlyLinkedList<Player>();
+        for (int i = roundNumber; i < playerListIndex.size(); i++) {
+            gamePlayerList.addLast(playerListIndex.get(i));
+        }
+
+        for (int i = 0; i < roundNumber; i++) {
+            gamePlayerList.addLast(playerListIndex.get(i));
+        }
+
+
+
+        while (gamePlayerList.size() > 0) {  // while the round is still going on
+
+        }
 
     }
 }
