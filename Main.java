33598217937c6acc06ce85525bfc307e47e5579e
JustinReed <jreed1@haverford.edu>
2022-04-22 11:18:55 -0400

    fixed toString for Card

--- Main.java
+++ Main.java
@@ -1,7 +1,18 @@
 public class Main {
     public static void main (String[] args) {
 
+        Player player1 = new Player();
+        Player player2 = new Player();
+        Player player3 = new Player();
+        Player player4 = new Player();
+
+        CircularlyLinkedList<Player> playerListIndex = new CircularlyLinkedList<Player>();
+        playerListIndex.addLast(player1);
+        playerListIndex.addLast(player2);
+        playerListIndex.addLast(player3);
+        playerListIndex.addLast(player4);
+
+        CircularlyLinkedList<Player> gamePlayerList =
+
     }
-
-
 }
