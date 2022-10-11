b6e7fc065403c4ca370363604912d93ce0854076
JustinReed <94082624+JustinReed@users.noreply.github.com>
2022-10-10 21:40:09 -0700

    Update SinglyLinkedList.java

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -1,78 +1,85 @@
 public class SinglyLinkedList<E> {
-    private Node<E> head;        // head node of the list (or null if empty)
-    private Node<E> tail;        // last node of the list (or null if empty)
-    private int size;               // number of nodes in the list
+/**
+* Singly Linked List implementation taken from slides
+*/
 
-    public SinglyLinkedList() {    // constructs an initially empty list
-        head = null;
-        tail = null;
-        size = 0;
-    }
 
-    /* continuation of Class SinglyLinkedList */
-    public int size() {
-        return size;
-    }
-    public boolean isEmpty() {
-        return size == 0;
-    }
-    public E first( ) {                 // returns (but does not remove) the first element
-        if (isEmpty( )) return null;
-        return head.getElement( );
-    }
+   private Node<E> head;        // head node of the list (or null if empty)
+   private Node<E> tail;        // last node of the list (or null if empty)
+   private int size;               // number of nodes in the list
 
-    public E last() {                  // returns (but does not remove) the last element
-        if (isEmpty( )) return null;
-        return tail.getElement( );
-    }
+   public SinglyLinkedList() {    // constructs an initially empty list
+       head = null;
+       tail = null;
+       size = 0;
+   }
 
-    // update methods
-    public void addFirst(E e) {         // adds element e to the front of the list
-        Node<E> newest = new Node<E>(e, null);
-        newest.setNext(head);
-        head = newest;
+   /* continuation of Class SinglyLinkedList */
+   public int size() {
+       return size;
+   }
+   public boolean isEmpty() {
+       return size == 0;
+   }
+   public E first( ) {                 // returns (but does not remove) the first element
+       if (isEmpty( )) return null;
+       return head.getElement( );
+   }
 
-        if (isEmpty())
-            tail = head;                // special case: new node becomes tail also
+   public E last() {                  // returns (but does not remove) the last element
+       if (isEmpty( )) return null;
+       return tail.getElement( );
+   }
 
-        size++;
-    }
-    public void addLast(E e) {          // adds element e to the end of the list
-        Node<E> newest = new Node<E>(e, null); // node will eventually be the tail
+   // update methods
+   public void addFirst(E e) {         // adds element e to the front of the list
+       Node<E> newest = new Node<E>(e, null);
+       newest.setNext(head);
+       head = newest;
 
-        if (isEmpty())                 // special case: previously empty list
-            head = newest;
-        else
-            tail.setNext(newest);       // new node after existing tail
+       if (isEmpty())
+           tail = head;                // special case: new node becomes tail also
 
-        tail = newest;                  // new node becomes the tail
-        size++;
-    }
+       size++;
+   }
+   public void addLast(E e) {          // adds element e to the end of the list
+       Node<E> newest = new Node<E>(e, null); // node will eventually be the tail
 
-    public E removeFirst() {            // removes and returns the first element
-        if (isEmpty( ))                 // nothing to remove
-            return null;
+       if (isEmpty())                 // special case: previously empty list
+           head = newest;
+       else
+           tail.setNext(newest);       // new node after existing tail
 
-        E answer = head.getElement( );
-        head = head.getNext( );         // will become null if list had only one node
-        size--;
+       tail = newest;                  // new node becomes the tail
+       size++;
+   }
 
-        if (size == 0)
-            tail = null;                // special case as list is now empty
+   public E removeFirst() {            // removes and returns the first element
+       if (isEmpty( ))                 // nothing to remove
+           return null;
 
-        return answer;
-    }
+       E answer = head.getElement( );
+       head = head.getNext( );         // will become null if list had only one node
+       size--;
 
-    public String toString() {
-        if (this.isEmpty()){
-            return "empty stack.";
-        }
-        Node<E> curr = head;
-        String str = head.toString();                   // Using toString from the Node Class
-        while(curr.getNext() != null){
-            curr = curr.getNext();
-            str = str + " -> " + curr.toString();       // Using toString from the Node Class
-        }
-        return str;
-    }
+       if (size == 0)
+           tail = null;                // special case as list is now empty
+
+       return answer;
+   }
+
+   public String toString() {
+       if (this.isEmpty()){
+           return "empty stack.";
+       }
+       Node<E> curr = head;
+       String str = head.toString();                   // Using toString from the Node Class
+       while(curr.getNext() != null){
+           curr = curr.getNext();
+           str = str + " -> " + curr.toString();       // Using toString from the Node Class
+       }
+       return str;
+   }
 }
+
+
